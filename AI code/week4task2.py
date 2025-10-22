7e58956746cd993ad4f9a5a5ea73c98c51adae6e
HunnyMun <minhanharmy2130@gmail.com>
2025-10-22 21:29:16 +0700

    Update week4task2.py

--- AI code/week4task2.py
+++ AI code/week4task2.py
@@ -5,7 +5,15 @@ import seaborn as sns
 import sklearn
 
 #TASK 2a
+
 # Load the dataset
 file_path = ''
 data = pd.read_csv(file_path)
+
+# Initial Inspection
+print("1. Initial Data Inspection")
+print("First 5 rows:")
+print(data.head())
+print("Data Info (Types & Not Null Counts:")
+data.info()
 
